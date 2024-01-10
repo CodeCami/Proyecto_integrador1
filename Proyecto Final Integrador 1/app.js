f20497279c8b9be890247736a001384e46377b27
CodeCami <156145397+CodeCami@users.noreply.github.com>
2024-01-09 21:07:43 -0300

    Add files via upload

---
+++ Proyecto Final Integrador 1/app.js
@@ -0,0 +1,26 @@
+//menu lateral
+var menu_visible = false;
+let menu = document.getElementById("nav");
+
+function mostrarOcultarMenu(){
+    if (menu_visible==false) {
+        menu.style.display= "block";
+        menu_visible = true;
+    }else{
+        menu.style.display= "none";
+        menu_visible = false;
+    }
+}
+
+//ocultar el menu una vez que selecciono una opcion
+
+let links = document.querySelectorAll("nav a");
+
+for (var x =0; x < links.length; x++){
+
+links[x].onclick =function(){
+    menu.style.display= "none";
+    menu_visible = false;
+}   
+
+}
